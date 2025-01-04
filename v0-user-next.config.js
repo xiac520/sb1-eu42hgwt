a17bac51bd9b0f2f013e9e2f74c1bb5ac3665328
xiac520 <love@xiac.cc>
2025-01-04 09:25:20 +0800

    1

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -5,30 +5,10 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  output: 'standalone',
+  output: 'export',
+  distDir: '.next',
   experimental: {
-    optimizeCss: true,
-    serverMinification: true,
-    serverSourceMaps: false,
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Production optimizations
-    if (!dev && isServer) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: true,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 24000000, // Keep chunks under 24MB
-          cacheGroups: {
-            default: false,
-            vendors: false,
-          }
-        }
-      }
-    }
-    return config
+    optimizeCss: true
   }
 }
 
